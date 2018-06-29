19be443c5fd22154286418fa4b5edd4f18815baa
James R. Moro <jamesrmoro@gmail.com>
2018-06-28 23:13:01 -0300

    Organização do js

---
+++ src/js/onda.js
@@ -0,0 +1,29 @@
+var canvas = document.getElementById('onda');
+var ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+//var date = Date.now();
+function draw(delta) {
+	requestAnimationFrame(draw);
+	canvas.width = canvas.width;
+	ctx.fillStyle = "#6dafe1";
+
+	var randomLeft = Math.abs(Math.pow( Math.sin(delta/1000), 2 )) * 100;
+	var randomRight = Math.abs(Math.pow( Math.sin((delta/1000) + 10), 2 )) * 100;
+	var randomLeftConstraint = Math.abs(Math.pow( Math.sin((delta/1000)+2), 2 )) * 100;
+	var randomRightConstraint = Math.abs(Math.pow( Math.sin((delta/1000)+1), 2)) * 100;
+
+	ctx.beginPath();
+	ctx.moveTo(0, randomLeft);
+
+    // ctx.lineTo(canvas.width, randomRight);
+    ctx.bezierCurveTo(canvas.width / 3, randomLeftConstraint, canvas.width / 3 * 2, randomRightConstraint, canvas.width, randomRight);
+    ctx.lineTo(canvas.width , canvas.height);
+    ctx.lineTo(0, canvas.height);
+    ctx.lineTo(0, randomLeft);
+    
+    ctx.closePath();
+    ctx.fill();
+}
+requestAnimationFrame(draw);
--- src/js/script.js
+++ src/js/script.js
@@ -119,38 +119,6 @@ $(document).ready(function(){
 			$(".conteudo").addClass("noite");
 		}
 
-		// Onda
-
-		var canvas = document.getElementById('onda');
-		var ctx = canvas.getContext('2d');
-		canvas.width = window.innerWidth;
-		canvas.height = window.innerHeight;
-
-		var date = Date.now();
-		function draw(delta) {
-			requestAnimationFrame(draw);
-			canvas.width = canvas.width;
-			ctx.fillStyle = "#6dafe1";
-
-			var randomLeft = Math.abs(Math.pow( Math.sin(delta/1000), 2 )) * 100;
-			var randomRight = Math.abs(Math.pow( Math.sin((delta/1000) + 10), 2 )) * 100;
-			var randomLeftConstraint = Math.abs(Math.pow( Math.sin((delta/1000)+2), 2 )) * 100;
-			var randomRightConstraint = Math.abs(Math.pow( Math.sin((delta/1000)+1), 2)) * 100;
-
-			ctx.beginPath();
-			ctx.moveTo(0, randomLeft);
-
-		    // ctx.lineTo(canvas.width, randomRight);
-		    ctx.bezierCurveTo(canvas.width / 3, randomLeftConstraint, canvas.width / 3 * 2, randomRightConstraint, canvas.width, randomRight);
-		    ctx.lineTo(canvas.width , canvas.height);
-		    ctx.lineTo(0, canvas.height);
-		    ctx.lineTo(0, randomLeft);
-		    
-		    ctx.closePath();
-		    ctx.fill();
-		}
-		requestAnimationFrame(draw);
-
 		var jnew = "game.json";
 		$.getJSON(jnew).done(function(data){
 			var retorno = '';
